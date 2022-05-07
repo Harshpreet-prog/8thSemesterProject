6e2a6211de7e6de51fa4c4edd65036434463502d
Harshpreet singh <harshpreettaank@gmail.com>
2022-05-07 11:13:34 +0530

    first directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='### Problem statement> Our (fictional) client is an IT educational institute. They have reached out to us has reach out with the following:"IT jobs and technologies keep evolving quickly. This makes our field to be one of the most interesting out there. But on the other hand, such fast development confuses our students. They do not know which skills they need to learn for which job. "Do I need to learn C++ to be a Data Scientist?" "Do DevOps and System admins use the same technologies?" "I really like JavaScript; can I use it in Data Analytics?" Those are some of the questions that our students ask. Could you please develop a data-driven solution for our students to answer such questions? They mostly want to understand the relationships between the jobs and the technologies. ',
+    author='Harshpreet Singh',
+    license='',
+)
